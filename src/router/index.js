ef8e6824c0aabaed3557ec5b2e5cb511679726a4
idward <idward@vip.sina.com>
2017-05-11 11:35:29 +0800

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,19 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import Home from '@/components/Home';
+
+Vue.use(Router);
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  }
+];
+
+const router = new Router({
+  routes: routes
+});
+
+export default router;
